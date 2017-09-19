a1f6a19a443a758cd2765451900848e6bf1e8c8d
bloodofden <bloodofden@gmail.com>
2017-09-19 21:56:12 +0300

    I don't know where I should flood the task. Let it be in master

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  let A = false;
+  let i,j;
+  
+  if(!str) return false;
+
+  str.split('').forEach((elem, index) => {
+
+	for(i = 0; i < bracketsConfig.length; i++){
+		for(j = 0; j < bracketsConfig[i].length; j++){
+			if(elem === bracketsConfig[i][j]){
+				if(!stack[0]){
+					stack.push(elem);
+				}
+				else {
+					if((j && (stack[stack.length - 1] === bracketsConfig[i][j - 1])) || 
+					((bracketsConfig[i][j] === bracketsConfig[i][j + 1]) && (bracketsConfig[i][j] === stack[stack.length - 1])))
+						stack.pop();
+					else { 
+						stack.push(elem);
+					}
+				}
+				A = true;
+				break;
+			}
+		}
+		if(A) break;
+	}
+	A = false;
+
+  });
+
+  return stack.length ? false : true;
 }
